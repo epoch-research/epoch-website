72f08161ded0c8c1b9d8725793ab0f8a2d4444ad
Eirol <74545840+eirol@users.noreply.github.com>
2022-05-05 22:26:43 +0200

    Deployment testing

--- .github/actions/test/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -l
-
-echo "Hello $1"
-echo ${GITHUB_WORKSPACE}
-ls -l ${GITHUB_WORKSPACE}/..
-ls -l ${GITHUB_WORKSPACE}/../jekyll_build
-time=$(date)
-echo "::set-output name=time::$time"
