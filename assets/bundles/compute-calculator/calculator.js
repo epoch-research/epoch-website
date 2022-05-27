238e71530f3f62ecaa04558e5c322a4de0ff4d20
Eirol <74545840+eirol@users.noreply.github.com>
2022-05-27 14:39:34 +0200

    Team page changes

--- assets/bundles/compute-calculator/calculator.js
+++ assets/bundles/compute-calculator/calculator.js
@@ -1,5 +1,7 @@
 'use strict';
 
+console.log("I wouldn't look at the current code");
+
 function buildComputeCalculator(hardwareDataUrl) {
   let showFillOptions = true;
   let showFillInMessage = false;
